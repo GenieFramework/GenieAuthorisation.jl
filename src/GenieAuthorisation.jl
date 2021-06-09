ff5c9e1c0588e19549780318eaa852d14290c9dd
Adrian Salceanu <essenciary@users.noreply.github.com>
2021-06-09 17:30:39 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/GenieAuthorisation.jl
@@ -0,0 +1,5 @@
+module GenieAuthorisation
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GenieAuthorisation
+using Test
+
+@testset "GenieAuthorisation.jl" begin
+    # Write your tests here.
+end
